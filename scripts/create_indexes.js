d55b2145fb80776097eb7c4e862ad81d60c6a60b
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-15 14:50:38 -0800

    Extract indexes and query plans from the scripts injecting them

--- scripts/create_indexes.js
+++ scripts/create_indexes.js
@@ -7,35 +7,12 @@ var collection = '', l = [];
 
 collection = 'sequences';
 
-l = [
-	{ "_id" : 1 },
-	{ "ir_project_sample_id" : 1 },
-
-	{ "v_call" : 1, "ir_project_sample_id" : 1 },
-	{ "j_call" : 1, "ir_project_sample_id" : 1 },
-	{ "d_call" : 1, "ir_project_sample_id" : 1 },
-
-	{ "junction_aa_length" : 1, "ir_project_sample_id" : 1 },
-
-	{ "jgene_gene" : 1, "ir_project_sample_id" : 1 },
-	{ "jgene_family" : 1, "ir_project_sample_id" : 1 },
-	{ "vgene_gene" : 1, "ir_project_sample_id" : 1 },
-	{ "vgene_family" : 1, "ir_project_sample_id" : 1 },
-	{ "dgene_gene" : 1, "ir_project_sample_id" : 1 },
-	{ "dgene_family" : 1, "ir_project_sample_id" : 1 },
-
-	{ "functional" : 1, "ir_project_sample_id" : 1 },
-	{ "ir_annotation_tool" : 1, "ir_project_sample_id" : 1 },
-
-	{ "substring" : 1, "ir_project_sample_id" : 1 }
-
-
-];
+load("indexes.js");
 
 /****************************************************************************************
  MAIN */
 
-l.forEach(function(obj, i) {
+indexes.forEach(function(obj, i) {
        var index = tojson(obj);
        t0 = new Date();
        
