b06fd30eefe7c9884a2a8a6a8073fba487e9a8db
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-03-26 16:18:02 -0700

    Make sure collection exists before creating query plans

--- scripts/create_query_plans.js
+++ scripts/create_query_plans.js
@@ -12,6 +12,10 @@ load("query_plans.js");
 /****************************************************************************************
  MAIN */
 
+// hack to make sure the collection exists
+db.[collection].createIndex({ "_id" : 1 });
+
+// create query plans
 query_plans.forEach(function(obj, i) {
     var response = db.runCommand(obj)
     printjson(response);
