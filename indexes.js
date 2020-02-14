88f7f58a5c8b15a8019dd9cf1dfcc133f821275f
Brian Corrie <bdcorrie@gmail.com>
2020-02-14 13:03:38 -0800

    Added indexes for repertoire_id (in addition to the ir_project_sample_id fields). Will get rid of the old ones later once things are working.

--- indexes.js
+++ indexes.js
@@ -18,5 +18,28 @@ indexes = [
 	{ "productive" : 1, "ir_project_sample_id" : 1 },
 	{ "ir_annotation_tool" : 1, "ir_project_sample_id" : 1 },
 
-	{ "ir_substring" : 1, "ir_project_sample_id" : 1 }
+	{ "ir_substring" : 1, "ir_project_sample_id" : 1 },
+
+
+
+
+	{ "repertoire_id" : 1 },
+
+	{ "v_call" : 1, "repertoire_id" : 1 },
+	{ "j_call" : 1, "repertoire_id" : 1 },
+	{ "d_call" : 1, "repertoire_id" : 1 },
+
+	{ "ir_junction_aa_length" : 1, "repertoire_id" : 1 },
+
+	{ "ir_jgene_gene" : 1, "repertoire_id" : 1 },
+	{ "ir_jgene_family" : 1, "repertoire_id" : 1 },
+	{ "ir_vgene_gene" : 1, "repertoire_id" : 1 },
+	{ "ir_vgene_family" : 1, "repertoire_id" : 1 },
+	{ "ir_dgene_gene" : 1, "repertoire_id" : 1 },
+	{ "ir_dgene_family" : 1, "repertoire_id" : 1 },
+
+	{ "productive" : 1, "repertoire_id" : 1 },
+	{ "ir_annotation_tool" : 1, "repertoire_id" : 1 },
+
+	{ "ir_substring" : 1, "repertoire_id" : 1 }
 ];
