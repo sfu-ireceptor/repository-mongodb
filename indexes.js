93a5a0e5e1d08173cfa02367e1ddc102bc1922d0
Brian Corrie <bdcorrie@gmail.com>
2020-02-18 09:39:25 -0800

    Updated for new AIRR data model and new AIRR Mapping. No more ir_project_sample_id, replaced with repertoire_id.

--- indexes.js
+++ indexes.js
@@ -1,27 +1,5 @@
 indexes = [
 	{ "_id" : 1 },
-	{ "ir_project_sample_id" : 1 },
-
-	{ "v_call" : 1, "ir_project_sample_id" : 1 },
-	{ "j_call" : 1, "ir_project_sample_id" : 1 },
-	{ "d_call" : 1, "ir_project_sample_id" : 1 },
-
-	{ "ir_junction_aa_length" : 1, "ir_project_sample_id" : 1 },
-
-	{ "ir_jgene_gene" : 1, "ir_project_sample_id" : 1 },
-	{ "ir_jgene_family" : 1, "ir_project_sample_id" : 1 },
-	{ "ir_vgene_gene" : 1, "ir_project_sample_id" : 1 },
-	{ "ir_vgene_family" : 1, "ir_project_sample_id" : 1 },
-	{ "ir_dgene_gene" : 1, "ir_project_sample_id" : 1 },
-	{ "ir_dgene_family" : 1, "ir_project_sample_id" : 1 },
-
-	{ "productive" : 1, "ir_project_sample_id" : 1 },
-	{ "ir_annotation_tool" : 1, "ir_project_sample_id" : 1 },
-
-	{ "ir_substring" : 1, "ir_project_sample_id" : 1 },
-
-
-
 
 	{ "repertoire_id" : 1 },
 
@@ -29,7 +7,7 @@ indexes = [
 	{ "j_call" : 1, "repertoire_id" : 1 },
 	{ "d_call" : 1, "repertoire_id" : 1 },
 
-	{ "ir_junction_aa_length" : 1, "repertoire_id" : 1 },
+	{ "junction_aa_length" : 1, "repertoire_id" : 1 },
 
 	{ "ir_jgene_gene" : 1, "repertoire_id" : 1 },
 	{ "ir_jgene_family" : 1, "repertoire_id" : 1 },
@@ -39,7 +17,6 @@ indexes = [
 	{ "ir_dgene_family" : 1, "repertoire_id" : 1 },
 
 	{ "productive" : 1, "repertoire_id" : 1 },
-	{ "ir_annotation_tool" : 1, "repertoire_id" : 1 },
 
 	{ "ir_substring" : 1, "repertoire_id" : 1 }
 ];
