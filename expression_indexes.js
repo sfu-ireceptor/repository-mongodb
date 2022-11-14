8636d505ad2b72702e7c21851eac16f5e41b4b9d
bcorrie <bdcorrie@gmail.com>
2022-11-14 19:14:45 +0000

    Add index required for linking cells/expression.

--- expression_indexes.js
+++ expression_indexes.js
@@ -11,6 +11,6 @@ indexes = [
 	{ "cell_id" : 1, "ir_annotation_set_metadata_id_expression" : 1 },
 	{ "property" : 1, "ir_annotation_set_metadata_id_expression" : 1 },
 	{ "property_id" : 1, "ir_annotation_set_metadata_id_expression" : 1 },
-	{ "value" : 1, "ir_annotation_set_metadata_id_expression" : 1 }
-	
+	{ "value" : 1, "ir_annotation_set_metadata_id_expression" : 1 },
+	{ "property" : 1, "repertoire_id" : 1 }
 ];
