a2d2c411049a568e18cafbeaeae07dc4f12c54ef
bcorrie <bdcorrie@gmail.com>
2025-08-11 19:05:48 +0000

    Add reactivity reference indexes.

--- indexes.js
+++ indexes.js
@@ -26,5 +26,9 @@ indexes = [
 	{ "consensus_count" : 1, "repertoire_id" : 1 },
 
 	{ "junction_aa" : 1, "repertoire_id" : 1 },
-	{ "ir_substring" : 1, "repertoire_id" : 1 }
+	{ "ir_substring" : 1, "repertoire_id" : 1 },
+
+	{ "ir_antigen_ref" : 1, "repertoire_id" : 1 },
+	{ "ir_species_ref" : 1, "repertoire_id" : 1 },
+	{ "ir_epitope_ref" : 1, "repertoire_id" : 1 }
 ];
